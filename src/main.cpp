2461dffb6f7af17c7a164e215e463fa1b19d9d18
Joshua Si <joshuasi777@gmail.com>
2022-12-30 14:50:37 -0800

    Add Game.h/Game.cpp source files

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,5 @@
-#include <SFML/Graphics.hpp>
-
+#include "Game.h"
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
-
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
-
-        window.clear();
-        window.display();
-    }
+	Game g = Game(60);
 }
